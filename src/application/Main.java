5750bbd6fd47342a2e90e0e7d42cd5fdb684db6c
JhonLSN <jhonatanluansantos@gmail.com>
2022-06-12 13:18:46 -0300

    Implementing window switching and UI/UX improvements

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,22 +7,51 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
 
 
 public class Main extends Application {
+	
+	private static Stage stage;	
+	private static Scene mainScene;
+	private static Scene interfaceScene;
+	
 	@Override
 	public void start(Stage stage) {
+		Main.stage = stage;
+		
 		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("/gui/Interface.fxml"));
-			Scene scene = new Scene(parent);
-			stage.setScene(scene);
 			stage.setTitle("Login");
-			stage.initStyle(StageStyle.UNDECORATED);
+			// Remove buttons from window
+			// stage.initStyle(StageStyle.UNDECORATED);
+
+			Parent fxmlInterface = FXMLLoader.load(getClass().getResource("/gui/Interface.fxml"));
+			interfaceScene = new Scene(fxmlInterface);
+			
+			Parent fxmlMain = FXMLLoader.load(getClass().getResource("/gui/Main.fxml"));
+			mainScene = new Scene(fxmlMain);
+			
+			// Disable custom window
 			stage.setResizable(false);
+			
+			// Calling main screen
+			stage.setScene(mainScene);
 			stage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	// Method to change screen
+	public static void changeScreen(String src) {
+		switch (src) {
+			case "main":
+				stage.setScene(mainScene);
+				stage.setTitle("Login");
+				break;
+			case "interface":
+				stage.setScene(interfaceScene);
+				stage.setTitle("BankTCS");
+				break;
 		}
 	}
 	
---
+++ src/gui/InterfaceController.java
@@ -0,0 +1,85 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class InterfaceController implements Initializable {
+	
+  @FXML
+  private Button btnAccount;
+
+  @FXML
+  private Button btnExit;
+
+  @FXML
+  private Button btnSettings;
+
+  @FXML
+  private Button btnTransfer;
+  
+  @FXML
+  private GridPane gpnAccount;
+
+  @FXML
+  private GridPane gpnSettings;
+
+  @FXML
+  private GridPane gpnTransfer;
+
+  @FXML
+  private Label lblStatus;
+
+  @FXML
+  private Label lblStatusMini;
+
+  @FXML
+  private Pane pnlStatus;
+  
+  @FXML
+  private VBox vbxStatus;
+  
+  @FXML
+  private Label lblLogo;
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {		
+	}
+	
+	@FXML
+	private void handleClicks(ActionEvent event) {
+		if (event.getSource() == btnAccount) {	
+			lblStatusMini.setText("Home/Account");
+			lblStatus.setText("Account");
+			pnlStatus.setBackground(new Background(new BackgroundFill(Color.web("#5c0e0e"), CornerRadii.EMPTY, Insets.EMPTY)));
+			gpnAccount.toFront();
+		} else if (event.getSource() == btnTransfer) {
+			lblStatusMini.setText("Home/Transfer");
+			lblStatus.setText("Transfer");
+			pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(43, 63, 99), CornerRadii.EMPTY, Insets.EMPTY)));
+			gpnTransfer.toFront();
+		} else if (event.getSource() == btnSettings) {
+			lblStatusMini.setText("Home/Settings");
+			lblStatus.setText("Settings");
+			pnlStatus.setBackground(new Background(new BackgroundFill(Color.rgb(43, 99, 63), CornerRadii.EMPTY, Insets.EMPTY)));
+			gpnSettings.toFront();
+		} else if (event.getSource() == btnExit) {
+				Main.changeScreen("main");	
+		}
+	}
+
+}
