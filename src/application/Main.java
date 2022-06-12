558da144fa8f1d982106f89ed99804b70426d0b6
JhonLSN <jhonatanluansantos@gmail.com>
2022-06-11 22:35:59 -0300

    Creating JavaFX interface screen

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,16 +7,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage stage) {
 		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
+			Parent parent = FXMLLoader.load(getClass().getResource("/gui/Interface.fxml"));
 			Scene scene = new Scene(parent);
 			stage.setScene(scene);
-			stage.setScene(scene);
+			stage.setTitle("Login");
+			stage.initStyle(StageStyle.UNDECORATED);
 			stage.setResizable(false);
 			stage.show();
 		} catch (IOException e) {
